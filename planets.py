49fe5eca3d8455cfdef27a802ecce13e07a29b3c
Ryan Fay <rmfay@calpoly.edu>
2018-09-23 10:27:09 -0700

    made Earth lowercase and got rid of new line

--- planets.py
+++ planets.py
@@ -1,8 +1,8 @@
 def weight_on_planets():
    # write your code here
-   weight = int(input("What do you weigh on Earth? "))
+   weight = int(input("What do you weigh on earth? "))
 
-   print("\n\nOn Mars you would weigh", weight * 0.38, "pounds.")
+   print("\nOn Mars you would weigh", weight * 0.38, "pounds.")
    print("On Jupiter you would weigh", weight * 2.34, "pounds.")
    
    
